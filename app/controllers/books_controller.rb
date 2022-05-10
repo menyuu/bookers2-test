286f5a68e6b499029a33cf53b059b69d51ac4e20
yuu <meny1122men@gmail.com>
2022-05-10 06:17:01 +0000

    new・edit・update・destroyの追加とredirect・render先の設定

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,35 +1,60 @@
 class BooksController < ApplicationController
-  
+
   def index
+    @books = Book.all
   end
-  
+
 
   def show
     @book = Book.find(params[:id])
   end
-  
-  
+
+
   def new
     @book = Book.new
+    @user = User.find(current_user.id)
   end
-  
-  
+
+
   def create
     @book = Book.new(book_params)
     @book.user_id = current_user.id
-    @book.save
-    redirect_to book_path(@book.id)
+    if @book.save
+      flash[:notice] = "You have created book successfully."
+      redirect_to book_path(@book.id)
+    else
+      render "books/index"
+    end
   end
-  
+
 
   def edit
+    @book = Book.find(params[:id])
   end
   
   
-  private
+  def update
+    @book = Book.find(params[:id])
+    if @book.update(book_params)
+      flash[:notice] = "You have updated book successfully."
+      redirect_to book_path(@book.id)
+    else
+      render :edit
+    end
+  end
   
+  
+  def destroy
+    @book = Book.find(params[:id])
+    @book.destroy
+    redirect_to books_path
+  end
+
+
+  private
+
   def book_params
     params.require(:book).permit(:title, :body)
   end
-  
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   
-  resources :books, only: [:index, :show, :new, :create, :edit]
+  resources :books, only: [:index, :show, :new, :create, :edit, :update, :destroy]
   
   devise_for :users
   
