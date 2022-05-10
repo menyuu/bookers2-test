2027685b7c18d778c350f7f190c3e8244e792160
yuu <meny1122men@gmail.com>
2022-05-10 11:56:28 +0000

    Bootstrapの導入

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -9,7 +9,12 @@ import * as ActiveStorage from "@rails/activestorage"
 import "channels"
 
 
-import '@fortawesome/fontawesome-free/js/all'
+import '@fortawesome/fontawesome-free/js/all';
+
+import "jquery";
+import "popper.js";
+import "bootstrap";
+import "../stylesheets/application" ;
 
 
 Rails.start()
